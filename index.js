ae73b2a69cb91443f199cb663d664cba14350864
TrashedBot <35202688+TrashedBot@users.noreply.github.com>
2021-02-02 12:01:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,43 @@ function initMap() {
       {featureType: 'water',elementType: 'labels.text.stroke',stylers: [{color: '#17263c'}]}
     ]
   });
-  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
+  script.src ="https://script.googleusercontent.com/macros/echo?user_content_key=CQ0bv7da2Xb80wG6a0KOqKjiT41nRwVR5AR5U7m0p7bC7DYLPAGZrJJdapWZKSrm0Xd4LseGPKX4ske4bs63ubedkNlXtZdim5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnAXG9WzXz78MuCDB8u3tvlV_99N1TkWyPKKX-BCeLI2ljxtHvgA_5oPLMmlKaPz_Y-70lrzdhBYLGGaU2pP6Ntk&lib=Mc8KzVU3_s1AkC74h0iWLBHC7tyB-4zAN";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+const location = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+    new google.maps.Marker({
+      position: latLng,
+      map: map,});
+  }
+};
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /* Create a <script> tag and set the USGS URL as the source.
   const script = document.createElement("script");
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
@@ -48,4 +84,5 @@ const eqfeed_callback = function (results) {
     });
   }
 };
+*/
 // [END maps_earthquake_markers]
